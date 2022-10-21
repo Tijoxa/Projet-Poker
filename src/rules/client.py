723a8bf4b61d0e58f3c6ea953680da50b278718d
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2022-10-21 16:54:38 +0200

    Update client.py

--- src/rules/client.py
+++ src/rules/client.py
@@ -1,10 +1,12 @@
 class Client:
     """
     Le client est grossièrement le joueur. Le code comprendra les actions suivantes :
-    - call (suivre, s'aligner sur la mise précédente. SI pas assez d'argent all in et création d'un side pot)
+    - call (suivre, s'aligner sur la mise précédente. Si pas assez d'argent all in et création d'un side pot)
     - check (check, quand ta mise est égale à la mise générale ne rien faire)
     - fold (se coucher)
     - bet (miser, quand la mise est à 0, au minimum la grosse blinde)
     - raise (relancer: première relance : le double; après : relance d'au moins la différence en plus)
 
     """
+    def __init__(self) -> None:
+        pass
