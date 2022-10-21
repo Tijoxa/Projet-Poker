6f629a0245ef0eb48c15d3b0ae633117d32791fb
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2022-10-21 17:33:54 +0200

    Create server.py

---
+++ src/rules/server.py
@@ -0,0 +1,14 @@
+import socket
+import threading
+
+class Server:
+    """
+    
+    """
+    def __init__(self) -> None:
+        pass
+
+    def run(self) -> None:
+        pass
+
+    # faire les règles du jeu et tout
