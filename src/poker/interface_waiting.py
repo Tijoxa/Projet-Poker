56c35eedda9d121162939d215e10e97cb947d213
AthanaseB <athanase.bouronopoulos@eleves.ec-nantes.fr>
2022-12-06 13:03:08 +0100

    interface_client

--- src/poker/interface_waiting.py
+++ src/poker/interface_waiting.py
@@ -4,7 +4,7 @@ from pygame.locals import *
 from interface_elements import *
 
 class GUI_waiting:
-    def __init__(self,server=None):
+    def __init__(self, client):
         pg.init()
         #create the window :
         self.waiting = pg.display.set_mode([1023, 510])
@@ -17,20 +17,33 @@ class GUI_waiting:
         #connected players : 
         self.list_players = []
 
+        my_player = pg.image.load('icons/player_basic_image.png')
+        self.player_icon = my_player.convert_alpha() # Pour gérer la transparence
+        self.player_icon = pg.transform.scale(self.player_icon,(120,100))
+
+        #connexion du joueur :
+        self.client = client
+
         
     def mainloop(self):
         clock = pg.time.Clock()
         #buttons :
         button_quit = Button(20, 30, 200, 50, text = "Quitter la salle")
         button_play = Button(800, 30, 200, 50, text = "Lancer la partie !")
-        button_add_player = Button(80, 360, 50, 50, shape = 'circle',
+        button_add_IA = Button(570, 375, 50, 50, shape = 'circle',
                                    colour_mouse_on = 'black', colour_mouse_off = 'green',
                                    textSize = 30, text = "+")
-        button_del_player = Button(240, 360, 50, 50, shape = 'circle',
+        button_del_IA = Button(380, 375, 50, 50, shape = 'circle',
+                                   colour_mouse_on = 'black',
+                                   textSize = 30, text = "-")
+        button_add_player = Button(600, 435, 50, 50, shape = 'circle',
+                                   colour_mouse_on = 'black', colour_mouse_off = 'green',
+                                   textSize = 30, text = "+")
+        button_del_player = Button(360, 435, 50, 50, shape = 'circle',
                                    colour_mouse_on = 'black',
                                    textSize = 30, text = "-")
         input_buttons = [button_quit,button_play, 
-                         button_add_player, button_del_player]
+                         button_add_player, button_del_player,button_add_IA, button_del_IA]
         done = False
 
         while not done:
@@ -38,22 +51,42 @@ class GUI_waiting:
                 if event.type == pg.QUIT:
                     done = True
                 for button in input_buttons :
+                    # CHECK THE POSITION OF THE MOUSE
+                    mouse_pos = pg.mouse.get_pos()
+                    # CHECKING THE MOUSE CLICK EVENT
+                    mouse_click = pg.mouse.get_pressed()
                     button.handle_event(event)
 
             self.waiting.blit(self.bg,(0,0))
             for button in input_buttons : 
                 button.draw(self.waiting)
                 
-            for player in self.list_players:
-                #affichage du pseudo de chaque joueur et une image dont la 
-                #couleur dépend de si c'est un humain ou une IA
-                ### TODO  : mise à jour des joueur avec les données serveur ###
-                pass
+            # Affichage des clients connectés
+            self.list_players = self.client.players[1:]
+            font_pseudo = pg.font.Font('freesansbold.ttf', 32)
+            for k in range (len(self.list_players)):
+                self.waiting.blit(self.player_icon, (50 + 160*k, 250)) # Affichage des icônes de personnage
+
+                name = self.list_players[k].split("-")[1] # Obtention du nom du client à afficher
+                text = font_pseudo.render(name, True, (0, 0, 128))
+                textRect = text.get_rect()
+                textRect.center = (100 + 150*k, 200)
+                self.waiting.blit(text, textRect)
+
+            # Réglage des joueurs attendus IA et réels 
+            font_number = pg.font.Font('freesansbold.ttf', 32)
+            N_IA_text = font_number.render(f"IAs : {str(self.client.N_players[1])}", True, (0, 0, 128))
+            N_real_text = font_number.render(f"Joueurs : {str(self.client.N_players[0])}", True, (0, 0, 128))
+            N_IA_textRect = N_IA_text.get_rect()
+            N_real_textRect = N_real_text.get_rect()
+            N_IA_textRect.center = (500, 400)
+            N_real_textRect.center = (500, 460)
+            self.waiting.blit(N_IA_text, N_IA_textRect)
+            self.waiting.blit(N_real_text, N_real_textRect)
 
 
             if button_quit.CurrentState:
                 button_quit.CurrentState = False
-                ### TODO : déconnexion du joueur du serveur ###
                 pg.quit()
                 return "HOME"
 
@@ -62,7 +95,24 @@ class GUI_waiting:
                 pg.quit()
                 return "PLAY"
 
+            if button_del_IA.CurrentState:
+                button_del_IA.CurrentState = False
+                self.client.N_players[1] = str(max(0,int(self.client.N_players[1]) -1)) 
+
+            if button_del_player.CurrentState:
+                button_del_player.CurrentState = False
+                self.client.N_players[0] = str(max(0,int(self.client.N_players[0]) -1))
+
+            if button_add_IA.CurrentState:
+                button_add_IA.CurrentState = False
+                self.client.N_players[1] = str(max(0,int(self.client.N_players[1]) +1))
+
+            if button_add_player.CurrentState:
+                button_add_player.CurrentState = False
+                self.client.N_players[0] = str(max(0,int(self.client.N_players[0]) +1))
+
             pg.display.flip()
+            pg.display.update()
             clock.tick(30)
         pg.quit()
         return ""
--- src/poker/main.py
+++ src/poker/main.py
@@ -1,21 +1,34 @@
 from interface_waiting import GUI_waiting
 from interface_home import GUI_homepage
 from interface_playRoom import GUI_playRoom
+from interface_client import Client_interface as Client
 
 if __name__ == "__main__":
-    gui = GUI_homepage()
-    screen = gui.mainloop()
     done = False
+    screen = "HOME"
+    connected = False # Placeholder pour limiter la création de multiples clients en alternant HOME et WAITING.
 
     while not done:
-        if screen == "WAITING":
-            gui = GUI_waiting()
-            screen = gui.mainloop()
-        elif screen == "HOME":
+        if screen == "HOME":
             gui = GUI_homepage()
+            screen, pseudo = gui.mainloop()
+            if not connected and screen == "WAITING" : 
+                # L'ajout de "screen == WAITING" protège d'une fermeture de la fenêtre qui provoquerait une connexion non désirée
+                client = Client(pseudo) 
+                # Création à la fin de gui.mainloop(), i.e. lorsque l'écran HOME est quitté
+                connected = True
+        elif screen == "WAITING":
+            print(f"Bienvenue, {str(pseudo)}")
+            gui = GUI_waiting(client)
             screen = gui.mainloop()
+            if screen == "HOME" : 
+                # Déconnexion de la salle d'attente lorsqu'on la quitte
+                client.quit() 
+                connected = False  
         elif screen == "PLAY":
-            gui = GUI_playRoom()
+            client.send("ready")
+            gui = GUI_playRoom(client)
             screen = gui.mainloop()
         elif screen == "":
+            client.quit() 
             done = True
