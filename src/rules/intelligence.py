9149522e9b1381cda1e910f98882e8e5a7c50f0d
EldritchGM <88211277+EldritchGM@users.noreply.github.com>
2023-03-08 14:05:50 +0100

    New AIs
    Introducing 3 new AI:
    - Gambler: alwayse rise
    - Caller: always call
    - Exponential Random Completer: determines how much he raises using exponential function.
    
    Observer.py is used to compare different AIs

--- src/rules/intelligence.py
+++ src/rules/intelligence.py
@@ -2,6 +2,8 @@ from random import randint, uniform, choices, sample
 import cards
 from itertools import combinations
 import cactus_evaluator
+import numpy as np
+import matplotlib.pyplot as plt
 class AI:
     def __init__(self, id):
         """
@@ -106,13 +108,16 @@ class Random_Completer(AI):
         return rate
 
     def decision(self):
+        rate = self.victory_rate()
+        return self.choix(rate)
+    
+    def choix(self, rate):
         if self.info["mise"] == 0:
             possible = ["CHECK", "MISE"]
         elif self.me["mise"] == self.info["mise"]:
             possible = ["CHECK", "RELANCE"]
         else:
             possible = ["COUCHER", "SUIVRE", "RELANCE"]
-        rate = self.victory_rate()
         choix = ""
         if rate < self.max_couche:
             choix = possible[0]
@@ -135,7 +140,141 @@ class Random_Completer(AI):
                 value = randint(mini_value, maxi_value)
                 choix = f"RELANCE {value}" 
         return choix#, rate    
+    
         
+class exp_Random_Completer(AI):
+    def __init__(self, id, depth = 100, hands_tested = 100):
+        super().__init__(id)
+        self.pseudo = "Random Completer"
+        self.depth = depth
+        self.hands_tested = hands_tested
+
+
+    def victory_rate(self):
+        hand = self.info["main"]
+        board = self.info["board"]
+        paquet = []
+        for symbol in cards.SYMBOLS:
+            for color in cards.COLORS:
+                paquet.append(symbol + color)
+        victories = 0
+        for card in (hand + board):
+            paquet.remove(card)
+        for _ in range(self.depth):
+            complete = sample(paquet, k=5 - len(board))
+            paquet_completion = paquet.copy()
+            for card in complete:
+                paquet_completion.remove(card)
+            complete = board + complete
+            score = cactus_evaluator.evaluate_7(*hand, *complete)
+            rng_hands = choices(list(combinations(paquet_completion, 2)), k = self.hands_tested)
+            rng_scores = [cactus_evaluator.evaluate_7(*rng_hand, *complete) for rng_hand in rng_hands]
+            victories += sum([score <= rng_score for rng_score in rng_scores])
+        rate = victories/(self.depth * self.hands_tested)
+        return rate
+
+    def decision(self):
+        rate = self.victory_rate()
+        accuracy = len(self.info["board"])
+        return self.choix(rate, accuracy)
+    
+    def choix(self, rate, accuracy):
+        if self.info["mise"] == 0:
+            possible = ["CHECK", "MISE"]
+        elif self.me["mise"] == self.info["mise"]:
+            possible = ["CHECK", "RELANCE"]
+        else:
+            possible = ["COUCHER", "SUIVRE", "RELANCE"]
+        choix = ""
+        val_choix = self.expo_choix(rate, accuracy)
+        if val_choix < 0: # se couche ou check
+            choix = possible[0]
+        elif val_choix == 0: # suit ou check
+            choix = possible[-2 + len(possible)]
+        else: # mise ou relance
+            choix = possible[-1]
+        
+        if choix == "MISE":
+            mini_value = min(self.info["blinde"], self.me["money"])
+            maxi_value = max(mini_value, round(0.1 * self.me["money"]))
+            value = val_choix * (maxi_value - mini_value) + mini_value
+            choix = f"MISE {value}"
+        if choix == "RELANCE":
+            mini_value = self.info["mise"] * 2
+            maxi_value = max(min(self.info["blinde"], self.me["money"]), self.me["mise"] + self.me["money"])
+            if mini_value > maxi_value:
+                choix = possible[-2 + len(possible)]
+            else:
+                value = val_choix * (maxi_value - mini_value) + mini_value
+                choix = f"RELANCE {value}" 
+        return choix#, rate    
+    
+    def expo_choix(self, rate, accuracy):
+        if accuracy == 0:
+            b_moins = 0.1
+            b_plus = 0.7
+            param = 5
+        elif accuracy == 5:
+            b_moins = 0.40
+            b_plus = 0.60
+            param = 3
+        else:
+            b_moins = 0.30
+            b_plus = 0.66
+            param = 1
+        if rate < b_moins:
+            return -1
+        elif rate < b_plus:
+            return 0
+        else:
+            return int(np.floor((np.exp(param * rate) - 1)/(np.exp(param) - 1)))
+        
+
+class Caller(AI):
+    def __init__(self, id):
+        super().__init__(id)
+        self.pseudo = "Caller"
+    
+    def decision(self):
+        if self.info["mise"] == 0:
+            possible = ["CHECK", "MISE"]
+        elif self.me["mise"] == self.info["mise"]:
+            possible = ["CHECK", "RELANCE"]
+        else:
+            possible = ["COUCHER", "SUIVRE", "RELANCE"]
+        return possible[-2 + len(possible)]
+
+
+# Deux IA triviales
+class Gambler(AI):
+    def __init__(self, id):
+        super().__init__(id)
+        self.pseudo = "Gambler"
+    
+    def decision(self):
+        if self.info["mise"] == 0:
+            possible = ["CHECK", "MISE"]
+        elif self.me["mise"] == self.info["mise"]:
+            possible = ["CHECK", "RELANCE"]
+        else:
+            possible = ["COUCHER", "SUIVRE", "RELANCE"]
+        choix = possible[-1]
+        if choix == "MISE":
+            mini_value = min(self.info["blinde"], self.me["money"])
+            maxi_value = max(mini_value, round(0.1 * self.me["money"]))
+            value = randint(mini_value, maxi_value)
+            choix = f"MISE {value}"
+        if choix == "RELANCE":
+            mini_value = self.info["mise"] * 2
+            maxi_value = max(min(self.info["blinde"], self.me["money"]), self.me["mise"] + round(0.2 * self.me["money"]))
+            if mini_value > maxi_value:
+                return possible[-2 + len(possible)]
+            else:
+                value = randint(mini_value, maxi_value)
+                choix = f"RELANCE {value}" 
+        return choix
+
+
 
 class PatrickCruel(AI):
     def __init__(self, id):
@@ -159,7 +298,15 @@ def ai(type, id, params = {}):
         min_lim_relance = params["min_lim_relance"]
         max_lim_relance = params["max_lim_relance"]
         return Random_Completer(id, depth, hands_tested, min_lim_couche, max_lim_couche, min_lim_relance, max_lim_relance)
-    
+    if type == "ERC":
+        depth = params["depth"]
+        hands_tested = params["hands_tested"]
+        return exp_Random_Completer(id, depth, hands_tested)
+    if type == "CL":
+        return Caller(id)
+    if type == "GB":
+        return Gambler(id)
+    raise ValueError(f"L'IA {type} n'existe pas.")
     
 if __name__ == "__main__":
     ai = Random_Completer(000, depth = 100, hands_tested = 100, min_lim_couche = 0.15, max_lim_couche = 0.35, min_lim_relance = 0.6, max_lim_relance = 0.8)
---
+++ src/rules/observer.py
@@ -0,0 +1,45 @@
+import server_trainer
+import intelligence
+import random
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+X = 0.26
+Y = 0.6
+host, port = ('localhost', 5566) # le 5566 a été paramétré par port forward sur ma machine pour être ouvert au réseau extérieur (pour le faire fonctionner chez vous il faut ouvrir le port 5566 sur les paramètres du routeur) 
+BLINDE = 2 # la petite blinde pour la partie
+MONEY = 100 # la monnaie de départ des joueurs
+NB_CLIENTS = 0 # le nombre de clients (joueurs humains)
+NB_IAS = 5 # le nombre d'IA
+server = server_trainer.Server((host, port),  NB_IAS)
+server.conns = [None] * NB_IAS
+NB_GAMES = 1000
+DEPTH = 100
+HANDS_PER_LEVEL = 100
+victory = [0] * NB_IAS
+ids = [0] * NB_IAS
+
+plt.bar(range(NB_IAS), victory, tick_label = ["naive", "RC", "ERC", "CL", "GB"])
+plt.ion()
+plt.show()
+for i in tqdm(range(NB_GAMES)):
+    server.conns[0] = server_trainer.AIThread(server, "naive")
+    server.conns[1] = server_trainer.AIThread(server, "RC", depth = DEPTH, hands_tested = HANDS_PER_LEVEL, min_lim_couche = 0.15, max_lim_couche = 0.35, min_lim_relance = 0.6, max_lim_relance = 0.8)
+    server.conns[2] = server_trainer.AIThread(server, "ERC", depth = DEPTH, hands_tested = HANDS_PER_LEVEL)
+    server.conns[3] = server_trainer.AIThread(server, "CL")
+    server.conns[4] = server_trainer.AIThread(server, "GB")
+    for i in range(NB_IAS): ids[i] = server.conns[i].id
+    coup, exec, winner = server.run(BLINDE, MONEY)
+    victory[ids.index(winner)] += 1
+    #print(f"Naif: {victory[0]} - RC: {victory[1]} - ERC: {victory[2]} - CL: {victory[3]} - GB: {victory[4]}", end = "\r")
+    plt.clf()
+    plt.bar(range(NB_IAS), victory, tick_label = ["naive", "RC", "ERC", "CL", "GB"])
+    plt.pause(0.2)
+    plt.ion()
+    plt.show()
+print(f"Naif: {victory[0]} - RC: {victory[1]} - ERC: {victory[2]} - CL: {victory[3]} - GB: {victory[4]}")
+plt.clf()
+plt.bar(range(NB_IAS), victory, tick_label = ["naive", "RC", "ERC", "CL", "GB"])
+plt.pause(0.2)
+plt.show()
+input()
